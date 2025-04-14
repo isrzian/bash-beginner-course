fabc94117f55761fa25ea24be2700bf60b2fc57c
isrzian <shadow-sergej@ya.ru>
2025-04-14 14:09:09 +0700

    ДЗ 1

---
+++ homework1.sh
@@ -0,0 +1,11 @@
+echo "Привет, мое имя ${USER}"
+
+echo "Сейчас я нахожусь в ${PWD}"
+
+echo "Мой домашний каталог - ${HOME}"
+
+echo "Моя ОС"
+cat /etc/os-release
+
+echo "Мое время работы"
+uptime
